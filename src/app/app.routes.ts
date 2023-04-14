43d167dd9f306b7fc12318a6e0e90457072163e8
nicolasdaldegan <nicolasdaldega@gmail.com>
2023-04-14 08:06:21 -0300

    meu primeiro commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'folder/inbox',
+    pathMatch: 'full',
+  },
+  {
+    path: 'folder/:id',
+    loadComponent: () =>
+      import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+];
