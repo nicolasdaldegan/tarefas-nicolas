19751780baac0c6b862303163d3f1d47f825feac
Pegasis12 <1A226859524@fumecacad.local>
2023-04-27 10:25:00 -0300

    sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,8 @@ import { IonicModule } from '@ionic/angular';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Favoritos', url: '/favoritos', icon: 'heart' },
+    { title: 'Sair', url: '/login', icon: 'exit'},
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
